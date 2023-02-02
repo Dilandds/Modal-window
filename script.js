47a00b30215c6925e8ff9fc95818bf0f9944bd20
Dilandds <e17379@eng.pdn.ac.lk>
2023-02-02 18:22:08 +0530

    Added functinality

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const btnOpenModel = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+for (let i = 0; i < btnOpenModel.length; i++) {
+  btnOpenModel[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+const removePop = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', removePop);
+overlay.addEventListener('click', removePop);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    removePop();
+  }
+});
